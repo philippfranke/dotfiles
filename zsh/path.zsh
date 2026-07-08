4e7723e98c6f593fe4b8cd8729be2fdb12927fcb
Philipp Franke <dev@philippfranke.com>
2026-07-08 10:55:32 +0200

    fix(go): use ~/.go as GOPATH and align vim-go bin path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,8 @@
-export PATH="$HOME/.local/bin:$HOME/go/bin:$PATH"
+# Go: GOPATH under ~/.go so `go install` and vim-go tools share one bin dir
+# (see vim/vimrc.symlink g:go_bin_path). ~/go/bin kept as a fallback so tools
+# installed there before this move stay on PATH until reinstalled.
+export GOPATH="$HOME/.go"
+export PATH="$HOME/.local/bin:$HOME/.go/bin:$HOME/go/bin:$PATH"
 
 # Homebrew's zsh 5.9.1 bottle bakes in an fpath that points at a
 # non-existent versioned functions dir (.../zsh/5.9/functions rather than
